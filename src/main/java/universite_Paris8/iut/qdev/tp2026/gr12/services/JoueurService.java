e9ba05714c89e4deec756ec7d4c7fdad12cb27d7
Anton Meimoun B2 <meimounanton@gmail.com>
2026-02-26 11:08:25 +0100

    modification du pom

--- src/main/java/universite_Paris8/iut/qdev/tp2026/gr12/services/JoueurService.java
+++ src/main/java/universite_Paris8/iut/qdev/tp2026/gr12/services/JoueurService.java
@@ -2,7 +2,7 @@ package universite_Paris8.iut.qdev.tp2026.gr12.services;
 
 import universite_Paris8.iut.qdev.tp2026.gr12.interfaces.IJoueurDAO;
 import universite_Paris8.iut.qdev.tp2026.gr12.entitites.JoueurDTO;
-import exception.*;
+import universite_Paris8.iut.qdev.tp2026.gr12.exceptions.*;
 
 import java.time.Year;
 
--- src/test/java/universite_Paris8/iut/qdev/tp2026/gr12/JoueurServiceTest.java
+++ src/test/java/universite_Paris8/iut/qdev/tp2026/gr12/JoueurServiceTest.java
@@ -2,9 +2,10 @@ package universite_Paris8.iut.qdev.tp2026.gr12;
 
 import universite_Paris8.iut.qdev.tp2026.gr12.interfaces.IJoueurDAO;
 import universite_Paris8.iut.qdev.tp2026.gr12.entitites.JoueurDTO;
-import exception.*;
+import universite_Paris8.iut.qdev.tp2026.gr12.exceptions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import universite_Paris8.iut.qdev.tp2026.gr12.services.JoueurService;
 
 import java.time.Year;
 
